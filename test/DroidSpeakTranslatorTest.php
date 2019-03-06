b8c48009cae9f977c270cb2e5f4c6ab25068852b
Arun Gimblett <arun.gimblett@pay4later.com>
2019-03-06 13:06:12 +0000

    updating dockerfile/compose to only run a php-cli image

---
+++ test/DroidSpeakTranslatorTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Translator\Test;
+
+use PHPUnit\Framework\TestCase;
+use Translator\DroidSpeakTranslator;
+use Translator\Translator;
+
+class DroidSpeakTranslatorTest extends TestCase
+{
+    /** @var Translator */
+    private $translator;
+
+    protected function setUp(): void
+    {
+        $this->translator = new DroidSpeakTranslator();
+    }
+
+    public function testItTranslateWithValidInput()
+    {
+        $input = '01000011 01100101 01101100 01101100 00100000 00110010 00110001 00111000 00110111';
+        $expected = 'Cell 2187';
+
+        $value = $this->translator->translate($input);
+
+        $this->assertEquals($expected, $value);
+    }
+
+
+}
